2b8f5f20500034ffea2062a32e7c9caceeb40f0e
Karthikeyan <28617804+Karthikeyangeo@users.noreply.github.com>
2021-09-01 08:13:42 +0530

    Add files via upload
    testing files for html forms

---
+++ script.js
@@ -0,0 +1,15 @@
+// HTML Forms with dom manipulation
+
+var fname = document.getElementById("fname").value;
+
+var lname = document.getElementById("lname").value;
+
+var address = document.getElementById("address").value;
+
+var pin = document.getElementById("pin").value;
+
+// var gender = document.querySelector('input[name="Gender"]:checked').value;
+
+function addvalue(){
+    console.log(fname,lname,address,pin);
+}
